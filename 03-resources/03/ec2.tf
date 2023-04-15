d770a489619efd8b444441783c2adb145ffcd11b
udaye <udayeswari.savana@gmail.com>
2023-04-15 14:33:23 -0400

    create insatnce

---
+++ 03-resources/03/ec2.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "sample" {
+  ami                    = "ami-0bb6af715826253bf"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
---
+++ 03-resources/03/sg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = vpc-0e2c181d0bd814f05
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
