4045f3caebb8cefad2230eb8bc7b078c0220fac7
udaye <udayeswari.savana@gmail.com>
2023-04-15 16:05:26 -0400

    functions

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -6,3 +6,19 @@ variable "sample" {
   default = ["abc",123]
 
 }
+
+variable "sample1" {
+  default = {
+      orange = {
+        name  = "orange"
+        count = 10
+      }
+      apple = {
+        name= "apple"
+        count = 100
+      }
+    }
+  }
+output "sample1" {
+  value = lookup(var.sample1, "apple",null)
+}
