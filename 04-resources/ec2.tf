edddf4f1686dcf8cd791d57d56397355e9acef23
udaye <udayeswari.savana@gmail.com>
2023-04-15 15:46:00 -0400

    s3 bucket

--- 04-resources/ec2.tf
+++ 04-resources/ec2.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "sample" {
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = upper(var.tagName)
+  }
 }
+variable "tagName"{}
