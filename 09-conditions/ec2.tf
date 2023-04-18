05951d631c0f471063439dfededb5d4ab2330fa3
udaye <udayeswari.savana@gmail.com>
2023-04-17 22:26:58 -0400

    loops

---
+++ 09-conditions/ec2.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "sample" {
+  ami                    = "ami-0bb6af715826253bf"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = aws_security_group.allow_ssh.*.id
+}
+
+tags = {
+  Name = "test"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  count       =var.create_sg ? 1 : 0
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0e2c181d0bd814f05"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+variable "create_sg" {
+  default = false
+}
