69afe7ef8b9c4ffda5d0080e056754beb75d3250
udaye <udayeswari.savana@gmail.com>
2023-04-17 21:17:34 -0400

    loops

--- 08-loops/01/main.tf
+++ 08-loops/01/main.tf
@@ -2,4 +2,7 @@ resource "aws_instance" "sample" {
   count                  = 2
   ami                    = "ami-0bb6af715826253bf"
   instance_type          = "t3.micro"
+  tags = {
+    Name = "test-${count.index}"
+  }
 }
